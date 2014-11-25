3d11cec0f84f300e774bd3d6196d4f5d25dfa6bb
Christian Pontesegger <christian.pontesegger@web.de>
2014-11-25 08:54:10 +0100

    fixed contributor
    Change-Id: I6a9c7a5487ca72693f9e432b14b1354d251d0a28

--- JavaScript Snippets/comment_out_selection.js
+++ JavaScript Snippets/comment_out_selection.js
@@ -2,13 +2,12 @@
  * name : Comment Out Selection with EASE
  * popup : enableFor(java.lang.Object)
  *
- * Copyright (c) 2014 Christian Pontesegger and others.
+ * Copyright (c) 2014 Paul D. Fernhout and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributed by: Paul D. Fernhout
  * Purpose: Comments out the current selection
  * Installation: Put this script to a monitored location (see Preferences/Scripting/Script Location) and it will be available in any context menu.
  * Scripts must be allowed to run on the UI thread for this to work (see Preferences/Scripting)
