0e902b2ec3d2f175409c97afa17c226b21afd795
Christian Pontesegger <christian.pontesegger@web.de>
2014-12-05 21:58:11 +0100

    disabled nashorn engine
      instanceof methods do not work on nashorn 8.0.25, disabled nashorn
      engine in general
    
    added adapter
    
      adapt selection to IResource
    
    Change-Id: Ia48866b73aace2062d51a11d917c2e598e3b64f5

--- JavaScript Snippets/Explore from here.js
+++ JavaScript Snippets/Explore from here.js
@@ -3,13 +3,14 @@
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors         : Christian Pontesegger - initial API and implementation
  * name                 : Explore from here
- * image				: platform:/plugin/org.eclipse.ease.ui/icons/eobj16/folder.png
- * toolbar				: Project Explorer
- * popup				: enableFor(org.eclipse.core.resources.IResource)
+ * image                : platform:/plugin/org.eclipse.ease.ui/icons/eobj16/folder.png
+ * toolbar              : Project Explorer
+ * popup                : enableFor(org.eclipse.core.resources.IResource)
  * script-type          : JavaScript
+ * script-engine        : !org.eclipse.ease.javascript.nashorn
  * description          : Start a file browser using current selection.
  ******************************************************************************/
 
@@ -20,9 +21,12 @@ var selection = getSelection();
 if (selection instanceof org.eclipse.jface.viewers.IStructuredSelection)
 	selection = selection.getFirstElement();
 
+if (!(selection instanceof org.eclipse.core.resources.IResource))
+	selection = adapt(selection, org.eclipse.core.resources.IResource)
+
 if (selection instanceof org.eclipse.core.resources.IFile)
 	selection = selection.getParent();
 
 if (selection instanceof org.eclipse.core.resources.IContainer)
-	// runProcess("/usr/bin/krusader", ["--left", getSystemProperty("user.home"), "--right", selection.getRawLocation()]);
-	runProcess("explorer.exe", [ selection.getRawLocation() ]);
+	runProcess("/usr/bin/krusader", [ "--left", getSystemProperty("user.home"), "--right", selection.getRawLocation() ]);
+	//        runProcess("explorer.exe", [ selection.getRawLocation() ]);
